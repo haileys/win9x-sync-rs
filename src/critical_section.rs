8a91d80ba143d71a8b6696ed94afb05ff261f1d8
Hailey Somerville <hailey@hailey.lol>
2023-05-15 23:29:46 +1000

    add unsafe enter and leave APIs for CriticalSection

--- src/critical_section.rs
+++ src/critical_section.rs
@@ -15,15 +15,35 @@ impl CriticalSection {
         }
     }
 
-    pub fn enter(&'static self) -> SectionGuard {
+    /// Enters a critical section and return a guard object which
+    /// automatically leaves the critical section on `Drop`
+    pub fn lock(&'static self) -> SectionGuard {
         let ptr = self.crit.as_ptr() as *mut _;
 
         self.once.call(|| unsafe {
             winapi::InitializeCriticalSection(ptr);
         });
 
-        unsafe { winapi::EnterCriticalSection(ptr); }
+        unsafe { self.enter(); }
         SectionGuard { ptr }
+    }
+
+    /// Enters a critical section. This is a low-level API and should be
+    /// avoided in favour of `lock` if possible.
+    ///
+    /// It is up to the user to ensure this call is always paired with a
+    /// `leave` call. `enter` is not memory-unsafe itself, but it is marked
+    /// unsafe for parity with `leave`.
+    pub unsafe fn enter(&'static self) {
+        let ptr = self.crit.as_ptr() as *mut _;
+        unsafe { winapi::EnterCriticalSection(ptr); }
+    }
+
+    /// Leaves a critical section. Must only be called while currently in a
+    /// critical section entered with `enter`.
+    pub unsafe fn leave(&'static self) {
+        let ptr = self.crit.as_ptr() as *mut _;
+        unsafe { winapi::LeaveCriticalSection(ptr); }
     }
 }
 
